e3e3d8f1965c7b5809fa6d21020230d0d2f9d44a
rakazv <rakazv@outlook.com>
2024-10-05 01:44:39 +0900

    up

--- src/vmath.v
+++ src/vmath.v
@@ -2,6 +2,18 @@ module vmath
 
 import math
 
+@[noinit]
+pub struct NonZeroInt {
+	value int
+}
+
+pub fn NonZeroInt.new(n int) !NonZeroInt {
+	if n == 0 {
+		return error('Input must be a non-zero integer.')
+	}
+	return NonZeroInt{n}
+}
+
 // fract returns the fractional part of x.
 pub fn fract(x f64) f64 {
 	_, fract := math.modf(x)
--- src/vmath_test.v
+++ src/vmath_test.v
@@ -3,3 +3,7 @@ module vmath
 fn test_fract() {
 	assert fract(3.4) == 0.3999999999999999
 }
+
+fn test_non_zero_int() {
+	NonZeroInt.new(0) or { assert true }
+}
